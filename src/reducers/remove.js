60fa432dce9188f59eef79368dc95e8ee41b1ace
Daniel Cruz <d.cruz.489@hotmail.com>
2019-11-21 19:08:45 -0700

    finally - mvp

---
+++ src/reducers/remove.js
@@ -0,0 +1,22 @@
+import { ADD_ITEM, REMOVE_FEATURE } from '../actions/action';
+
+const initialState = {
+    removeFeatures: false
+}
+
+export function reducer(state = initialState, action) {
+    switch (action.type) {
+        case ADD_ITEM:
+            return {
+                ...state,
+                removeFeatures: !state.removeFeatures
+            }
+            case REMOVE_FEATURE:
+                return {
+                    ...state,
+                    feature: action.payload
+            }
+        default:
+            return state;
+    }
+} 
